1f504eefa5e6337bdf82b33c636f10476b02a240
asmuzsoy <amuzsoy@ncsu.edu>
2024-01-29 10:41:38 -0500

    attempted to add similarity loss

--- superraenn/raenn.py
+++ superraenn/raenn.py
@@ -21,6 +21,78 @@ ENCODING_N_DEFAULT = 10
 N_EPOCH_DEFAULT = 1000
 
 
+# need all of the input object IDs
+
+class SimilarityHelperLayer(Layer):
+    def __init__(self, **kwargs):
+
+        super(SimilarityHelperLayer, self).__init__(**kwargs)
+
+    def call(self, merged):
+
+        merged_i = tf.repeat(tf.expand_dims(merged[:,:,-1:], 0), tf.shape(merged[:,:,-1:])[0], axis=0) # timestamps
+        merged_j = tf.repeat(tf.expand_dims(merged[:,:,:-1], 0), tf.shape(merged[:,:,:-1])[0], axis=0) # latent space vals
+        merged_j = tf.transpose(merged_j, perm=[1,0,2,3])
+
+        merged_ij = tf.concat([merged_j, merged_i], axis=-1)
+
+        return merged_ij
+
+class SimilarityLossLayer(Layer):
+    """
+    Samples from the latent normal distribution using
+    reparametrization to maintain gradient propagation.
+
+    Parameters
+    ----------
+    samp_args : array
+        the mean and log sigma values of the latent space
+        distribution
+
+    Returns
+    ----------
+    sample : array
+        a sampled value from the latent space
+    """
+    def __init__(self, maxlen, bmin, bmax, **kwargs):
+
+        self.k1 = 20. #TODO: add cyclical annealing
+        self.k0 = 2.
+
+        super(SimilarityLossLayer, self).__init__(**kwargs)
+
+    def call(self, input1, samples):
+        # samples is the latent variables
+        # input1 is the entire input array (last row is object IDs)
+        #
+        # time_arr = merged[:,:,-1]
+        objids = input1[:,:,-1]
+
+        # calculate distance in latent space
+        # calculate what the "distances" are supposed to be
+        # get the difference
+        # average over elements
+        S_i = tf.repeat(tf.expand_dims(samples, 0), tf.shape(samples)[0], axis=0)
+        S_j = tf.transpose(S_i, perm=[1,0,2])
+        # This is the distance in latent space
+        S_ij = tf.reduce_mean(tf.math.square(S_i - S_j), axis=-1)
+        # make "adjacency matrix" type thing for object IDs
+        objid_mat = tf.repeat(tf.expand_dims(objids, 0), tf.shape(objids)[0], axis=0)
+
+        objid_bool_mat = tf.math.logical_not(tf.math.equal(objid_mat, tf.transpose(objid_mat)))
+
+        # Distance for object IDs is 0 if they're the same and 1 otherwise
+        objid_dist = tf.tensor(1.) * objid_bool_mat
+
+        # Compute distance between distances?
+        L_sim = tf.reduce_mean(tf.math.square(S_ij - objid_dist), axis=-1)
+
+        # do contrastive loss with two matrices (perhaps just the element wise mean)
+        self.add_metric(L_sim, "constrastive_loss")
+
+        return merged
+
+
 def customLoss(yTrue, yPred):
     """
     Custom loss which doesn't use the errors
@@ -32,6 +104,9 @@ def customLoss(yTrue, yPred):
     yPred : array
         Predicted flux values
     """
+
+    # Do I need to add the similarity loss here? I don't think
+    # so, since we've "added the metric", but I'm not sure
     return K.mean(K.square(yTrue[:, :, 1:5] - yPred[:, :, :]))
 
 
